aebc588232e48e3a01d5c5786613a2a403be3b7a
Liwer <liwermon@gmail.com>
2015-02-10 15:54:53 +0200

    added links

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 def full_title(page_title)
 base_title = "Rubi on Rails Tutorial Sample App"
-if page title.empty?
+if page_title.empty?
 base_title
 else
 "#{base_title} | #{page_title}"
